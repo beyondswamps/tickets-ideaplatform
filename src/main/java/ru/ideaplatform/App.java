6a918aaf0407f81a20d61e03896a65c46014ddf6
OlegZabolotnykh <beyondswamps@gmail.com>
2025-08-06 17:40:05 +0300

    Ticket class: LocalDate and LocalTime fields added. Added jackson dependencies for proper date deser. Exception messages more informative now.

--- src/main/java/ru/ideaplatform/App.java
+++ src/main/java/ru/ideaplatform/App.java
@@ -4,6 +4,8 @@ package ru.ideaplatform;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import ru.ideaplatform.identity.Ticket;
 import ru.ideaplatform.dto.TicketsDto;
 
@@ -15,16 +17,18 @@ public class App {
     public static void main(String[] args) {
         File jsonFile = new File(args[0]);
         ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        mapper.registerModule(new JavaTimeModule());
         TicketsDto tickets;
 
         try {
             tickets = mapper.readValue(jsonFile, TicketsDto.class);
         } catch (DatabindException de) {
-            throw new RuntimeException("Databind processing problem");
+            throw new RuntimeException("DatabindException: " + de.getMessage());
         } catch (StreamReadException sre) {
-            throw new RuntimeException("Stream processing or parsing problems");
-        } catch (IOException e) {
-            throw new RuntimeException("Input error");
+            throw new RuntimeException("StreamReadException: " + sre.getMessage());
+        } catch (IOException ioe) {
+            throw new RuntimeException("IOException: " + ioe.getMessage());
         }
 
         List<Ticket> ticketList = tickets.getTickets();
--- src/main/java/ru/ideaplatform/identity/Ticket.java
+++ src/main/java/ru/ideaplatform/identity/Ticket.java
@@ -1,22 +1,30 @@
 package ru.ideaplatform.identity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Ticket {
     String origin;
-    String origin_name;
+    String originName;
     String destination;
-    String destination_name;
-    String departure_date;
-    String departure_time;
-    String arrival_date;
-    String arrival_time;
+    String destinationName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
+    LocalDate departureDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
+    LocalTime departureTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
+    LocalDate arrivalDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
+    LocalTime arrivalTime;
     String carrier;
     Integer stops;
     Integer price;
